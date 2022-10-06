0ad62842b81662204c4064bd535cd749b30c5e1f
Gaboubou <gab-attal@hotmail.fr>
2022-10-06 22:09:25 +0200

    feat: add firebase to project and setup .env.local

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,42 @@
 import './App.css'
 import {Home} from "./pages/Home";
+import {Channel} from "./pages/Channel";
+import {getAnalytics} from "firebase/analytics";
+import {initializeApp} from "firebase/app";
+import {collection, getDocs, getFirestore} from "firebase/firestore/lite";
+import {useEffect} from "react";
 
 function App() {
+    useEffect(() => {
+        window.process = {
+            ...window.process,
+        };
+    }, []);
+
+    const firebaseConfig = {
+        apiKey: import.meta.env.VITE_API_KEY,
+        authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+        projectId: import.meta.env.VITE_PROJECT_ID,
+        storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+        messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+        appId: import.meta.env.VITE_APP_ID,
+        measurementId: import.meta.env.VITE_MEASUREMENT_ID
+    };
+    const app = initializeApp(firebaseConfig);
+    const db = getFirestore(app);
+    const analytics = getAnalytics(app);
+    async function getCities(db:any) {
+        const citiesCol = collection(db, 'test');
+        const citySnapshot = await getDocs(citiesCol);
+        const cityList = citySnapshot.docs.map(doc => doc.data());
+        console.log(cityList)
+        return cityList;
+    }
+
+    getCities(db)
     return (
         <div className="App">
-            <Home/>
+            <Channel/>
         </div>
     )
 }
